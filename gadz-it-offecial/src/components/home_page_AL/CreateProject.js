4d6ebae5eb5ed81f3ab378454e0f6cff4507b5c4
AMINHOW <aminsahara880@gmail.com>
2024-06-15 21:48:01 +0100

    c
    f

--- gadz-it-offecial/src/components/home_page_AL/CreateProject.js
+++ gadz-it-offecial/src/components/home_page_AL/CreateProject.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import './CreateProject.css'
 import asset_12gt from '../../assets/asset-12gt-1@2x.png';
 import asset_22gt from '../../assets/asset-22-1@2x.png';
 
+function useComponentWidth(ref) {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    function updateWidth() {
+      setWidth(ref.current.offsetWidth);
+    }
+
+    window.addEventListener('resize', updateWidth);
+    updateWidth(); // Initial check
+
+    return () => window.removeEventListener('resize', updateWidth);
+  }, [ref]);
+
+  return width;
+}
 
 
+function CreateProject() {
 
-class CreateProject extends Component {
-  render() {
-    return (
+  const componentRef = useRef(null);
+  const width = useComponentWidth(componentRef);
+
+  return (
+    
       <div class="divmat-mdc-tab-body-wrapper1">
         <div class="tabpanel1">
           <div class="divmat-mdc-tab-body-content1">
-            <div class="divgrid1">
+            <div ref={componentRef} className={width < 1350 ? 'divgrid2' : 'divgrid1'}>
               <div class="divflex34">
                 <div class="start-up-your">Start up your project!</div>
                 <div className='limk10-parent'>
@@ -40,8 +59,8 @@ class CreateProject extends Component {
           </div>
         </div>
       </div>
-    )
-  }
+  )
 }
+
 
 export default CreateProject;
